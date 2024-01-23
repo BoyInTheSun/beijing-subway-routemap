b6186a34f46062850439d25d82ffc26c0390c308
BoyInTheSunm <18710284779@126.com>
2024-01-23 17:02:00 +0800

    demo

--- wwwroot/static/main.js
+++ wwwroot/static/main.js
@@ -1,4 +1,23 @@
 
+function hm2time(hm){
+    t = hm.split(':');
+    h = t[0];
+    m = t[1];
+    time = parseInt(h) * 60 + parseInt(m)
+    if (parseInt(h) <= 2){
+        time += 60 * 24;
+    }
+    return time;
+}
+function time2hm(time){
+    m = time % 60;
+    h = (time - m) / 60 % 24;
+    hm = String(h).padStart(2, '0') + ':' + String(m).padStart(2, '0');
+    return hm;
+}
+
+
+paths = Object();  // 存放path，{线路: [['M0 0 100 100', ...], ['反方向', ...]}}
 
 svg = document.getElementById("svg");
 
@@ -22,7 +41,12 @@ for (var i = 0; i < root.childElementCount; i++) {
     else {
         line_child_count = line.childElementCount;
     }
-    d = `M ${line.children[0].getAttribute("x")} ${line.children[0].getAttribute("y")}`; // 下个起点
+
+    paths[line_name] = Array();
+    paths[line_name].push(Array());
+    paths[line_name].push(Array());
+
+    d = `M${line.children[0].getAttribute("x")} ${line.children[0].getAttribute("y")}`; // 起点
     for (var j = 1; j < line_child_count; j++) {
         start_p = line.children[j - 1];
         end_p = line.children[j % line.childElementCount];
@@ -30,10 +54,10 @@ for (var i = 0; i < root.childElementCount; i++) {
 
         if (start_p.getAttribute("arc")) {
             arc = start_p.getAttribute("arc").split(':');
-            d_add = ` Q ${arc[0]} ${arc[1]} ${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`;
+            d_add = ` Q${arc[0]} ${arc[1]} ${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`;
         }
         else {
-            d_add = ` L ${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`;
+            d_add = ` L${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`;
         }
         d += d_add
 
@@ -41,17 +65,19 @@ for (var i = 0; i < root.childElementCount; i++) {
             continue
         }
 
-
+        paths[line_name][0].push(d);
         svg.append("path")
             .attr("stroke", line_color)
             .attr("fill", "none")
             .attr("d", d);
 
-        d = `M ${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`; // 下个起点
+        d = `M${end_p.getAttribute("x")} ${end_p.getAttribute("y")}`; // 下个起点
 
     }
+    // TODO: 计算反方向path，存到paths
     // TODO：画站
 
 }
 
+console.log(paths);
 console.log();
