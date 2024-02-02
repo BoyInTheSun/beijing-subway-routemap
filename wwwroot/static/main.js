82f4b24c367d627f65f75cee71cca0a4cbd35e49
BoyInTheSunm <18710284779@126.com>
2024-02-02 11:04:59 +0800

    显示时间

--- wwwroot/static/main.js
+++ wwwroot/static/main.js
@@ -17,11 +17,17 @@ function time2hm(time) {
     return hm;
 }
 
+function time2hms(time) {
+    s = Math.round((time % 1) * 60);
+    m = parseInt(time % 60);
+    h = parseInt(time / 60 % 24);
+    hms = String(h).padStart(2, "0") + ":" + String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0");
+    return hms;
+}
 
 paths = new Object();  // 存放path，{线路: [["M0 0 100 100", ...], ["反方向", ...]]}
 stations = new Object();  // 存放站名，{线路: [["站1", "站2", "..."], ...]}
 
-svg = d3.selectAll("#svg");
 
 var xhr_map = new XMLHttpRequest();
 xhr_map.open("GET", "static/map.xml", false);
@@ -130,62 +136,99 @@ xhr_sche_we.open("GET", "static/schedule_weekend.json", false);
 xhr_sche_we.send();
 sche_we = JSON.parse(xhr_sche_we.responseText);
 
+now_minute = 0;
+
 console.log(sche_wd);
 
+function get_now_minute(){
+    now = new Date()
+    return now.getHours() * 60 + now.getMinutes() + now.getSeconds() / 60 + now.getMilliseconds() / 60000;
+}
 
-//document.getElementsByTagNameNS();
+function start_set_time(begin_minute, show_minute, speed){
+    window.setInterval(`set_time(${begin_minute}, ${show_minute}, ${speed})`, 50);
+}
 
+function set_time(begin_minute, start_minute, speed){
+    time_now = get_now_minute();
+    time_offset = time_now - start_minute;
+    show_minute_now = begin_minute + time_offset * speed;
+    window.now_minute = show_minute_now;
+    time = time2hms(show_minute_now);
+    time_p = document.getElementById('time');
+    time_p.innerText = time;
+}
 // 画车动画
-begin_minute = 295;
-end_minute = 4320;
-speed = 60;
-transparency_second = 0.5;
+function draw_trains(begin_minute, end_minute, speed) {
+    transparency_second = 0.5;
+    xml_polygons = new Array();
+    for (line_name in sche_wd) {
+        direct_index = -1;
+        for (direct in sche_wd[line_name]) {
+            direct_index += 1;
+            console.log(direct_index, direct);
+            for (train_num in sche_wd[line_name][direct]) {
+                xml_animates = new Array();
 
-xml_polygons = new Array();
-for (line_name in sche_wd) {
-    direct_index = -1;
-    for (direct in sche_wd[line_name]) {
-        direct_index += 1;
-        console.log(direct_index, direct);
-        for (train_num in sche_wd[line_name][direct]) {
-            xml_animates = new Array();
-            for (var i = 0; i < sche_wd[line_name][direct][train_num].length - 1; i++) {
-                this_station_name = sche_wd[line_name][direct][train_num][i][0];
-                next_station_name = sche_wd[line_name][direct][train_num][i + 1][0];
-                this_time = sche_wd[line_name][direct][train_num][i][1];
-                next_time = sche_wd[line_name][direct][train_num][i + 1][1];
-                is_pass = false;
-                is_close = false;
-                if (this_time.indexOf('(') !== -1) {
-                    is_pass = true;
-                }
-                if (this_time.indexOf('-') !== -1) {
-                    index = stations[line_name][direct_index].indexOf(this_station_name);
-                    path = paths[line_name][direct_index][index].split(" ")[0];  // only Mx,y
-                    is_close = true;
-                }
-                else {
-                    index = stations[line_name][direct_index].indexOf(this_station_name);
-                    path = paths[line_name][direct_index][index];
-                }
-                this_time_minute = hm2time(this_time);
-                next_time_minute = hm2time(next_time);
-                if (begin_minute > this_time_minute) {
-                    continue;
-                }
-
+                // 淡入
+                this_time_minute = hm2time(sche_wd[line_name][direct][train_num][0][1]);
                 begin = parseInt((this_time_minute - begin_minute) * 60 / speed * 1000) + 'ms';
-                dur =parseInt((next_time_minute - this_time_minute) * 60 / speed * 1000) + 'ms';
-                xml_animates.push(`<animateMotion begin="${begin}" dur="${dur}" rotate="auto" path="${path}" repeatCount="1"></animateMotion>`)
-                
+                dur = parseInt((transparency_second) * 1000) + 'ms';
+                xml_animates.push(`<animate begin="${begin}" dur="${dur}" attributeName="opacity" values="0;1" repeatCount="1"></animate>`);
+
+                for (var i = 0; i < sche_wd[line_name][direct][train_num].length - 1; i++) {
+                    this_station_name = sche_wd[line_name][direct][train_num][i][0];
+                    next_station_name = sche_wd[line_name][direct][train_num][i + 1][0];
+                    this_time = sche_wd[line_name][direct][train_num][i][1];
+                    next_time = sche_wd[line_name][direct][train_num][i + 1][1];
+                    is_pass = false;
+                    is_close = false;
+                    if (this_time.indexOf('(') !== -1) {
+                        is_pass = true;
+                    }
+                    if (this_time.indexOf('-') !== -1) {
+                        index = stations[line_name][direct_index].indexOf(this_station_name);
+                        path = paths[line_name][direct_index][index].split(" ")[0];  // only Mx,y
+                        is_close = true;
+                    }
+                    else {
+                        index = stations[line_name][direct_index].indexOf(this_station_name);
+                        path = paths[line_name][direct_index][index];
+                    }
+                    this_time_minute = hm2time(this_time);
+                    next_time_minute = hm2time(next_time);
+                    if (begin_minute > next_time_minute) {
+                        continue;
+                    }
+
+                    begin = parseInt((this_time_minute - begin_minute) * 60 / speed * 1000) + 'ms';
+                    dur = parseInt((next_time_minute - this_time_minute) * 60 / speed * 1000) + 'ms';
+                    xml_animates.push(`<animateMotion begin="${begin}" dur="${dur}" rotate="auto" path="${path}" repeatCount="1"></animateMotion>`)
+
+                }
+
+                // 淡出
+                begin = parseInt(((next_time_minute - begin_minute) * 60 / speed - transparency_second) * 1000) + 'ms';
+                dur = parseInt((transparency_second) * 1000) + 'ms';
+                xml_animates.push(`<animate begin="${begin}" dur="${dur}" attributeName="opacity" values="1;0" repeatCount="1"></animate>`)
+
+                xml_polygon = `<polygon id="T_${train_num}" points="0,0 9,0 3,6 -9,6 -9,0" stroke-width="2" fill="grey" stroke="#790000">${xml_animates.join('')}</polygon>`;
+                xml_polygons.push(xml_polygon);
             }
-            xml_polygon = `<polygon id="T_${train_num}" points="0,0 9,0 3,6 -9,6 -9,0" stroke-width="2" fill="grey" stroke="#790000">${xml_animates.join('')}</polygon>`;
-            xml_polygons.push(xml_polygon);
         }
     }
-}
-svg.html(svg.html() + xml_paths.join('') + xml_polygons.join(''));
 
+    //svg = document.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'svg');
+    //svg.innerHTML = xml_paths.join('') + xml_polygons.join('');
+
+    svg = d3.selectAll("#svg");
+    svg.html(xml_paths.join('') + xml_polygons.join(''));
+
+    start_set_time(begin_minute, get_now_minute(), speed);
+
+}
+draw_trains(get_now_minute(), 4320, 30);
+/*
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -197,7 +240,7 @@ async function test() {
     }
 }
 test;
-/*
+
 async function write_animate() {
 
     for (var i = 296; i < 1500; i++) {
@@ -224,4 +267,4 @@ async function write_animate() {
 }
 //write_animate();
 */
-console.log();
+//console.log();
