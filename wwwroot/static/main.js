87edd2074bcd569ef8ef02285d8ce1d5a84f112f
BoyInTheSun <boyinthesun@163.com>
2024-02-25 11:53:15 +0800

    地图画完

--- wwwroot/static/main.js
+++ wwwroot/static/main.js
@@ -16,7 +16,7 @@ function time2hm(time) {
     hm = String(h).padStart(2, "0") + ":" + String(m).padStart(2, "0");
     return hm;
 }
-function time2h_m_s(time){
+function time2h_m_s(time) {
     s = Math.round((time % 1) * 60);
     m = parseInt(time % 60);
     h = parseInt(time / 60 % 24);
@@ -27,24 +27,90 @@ function time2hms(time) {
     hms = String(h).padStart(2, "0") + ":" + String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0");
     return hms;
 }
+function add(a, b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+var svg_style = `<style>
+    path.line {
+        fill: none;
+        stroke-linejoin: bevel;
+        stroke-linecap: round;
+        stroke-width: 3;
+    }
+    text.station-name {
+        fill: #000000;
+        font-size: 12px;
+    }
+    text.line-name {
+        text-anchor: middle;
+        dominant-baseline: central;
+        font-size: 12px;
+        fill: #ffffff;
+    }
+    circle.station-normal {
+        r: 3px;
+        fill: #ffffff;
+        stroke-width: 1px;
+        stroke-linecap: round;
+        stroke: #000000;
+    }
+    circle.station-transfer {
+        r: 5px;
+        fill: #ffffff;
+        stroke-width: 1px;
+        stroke-linecap: round;
+        stroke: #000000;
+    }
+</style>
+<symbol id="station-transfer">
+    <circle cx="5.5" cy="5.5" r="5" stroke-width="1" stroke="black" fill="white" />
+    <image xlink:href="/static/transfer.png" x="2.5" y="2.5" width="6" height="6" />
+</symbol>`
 
 var paths = new Object();  // 存放path，{线路: [["M0 0 100 100", ...], ["反方向", ...]]}
 var stations = new Object();  // 存放站名，{线路: [["站1", "站2", "..."], ...]}
+var stations_xy = new Object();  // 存放站名坐标，{线路: {"站1": ["x", "y"], "站2": ["x", "y"], ...}}
+var ex_stations_xy = new Object();  // 存放换乘站坐标，{"站1": [["x0", "y0"], ["x1", "y1"]], "站2": [[...], [...], [...]], ...}
+var close_stations = {  // TODO 需要体现站点关闭和虚拟换乘、某线路未开通站等
+    '蓟门桥': '暂缓开通',
+    '蓟门桥': '暂缓开通',
+    '蓟门桥': '暂缓开通',
+    '蓟门桥': '暂缓开通',
+    '蓟门桥': '暂缓开通',
+}
 
 
 var xhr_map = new XMLHttpRequest();
-xhr_map.open("GET", "static/map.xml", false);
+xhr_map.open("GET", "data/map.xml", false);
 xhr_map.send();
 var root = xhr_map.responseXML.getElementsByTagName("sw")[0];  // 根节点
 var line_colors = new Object();
-xml_paths = new Array();
+
+//算path，station
+var xml_paths = new Array();
+var xml_stationnames = new Array();
+var xml_stations = new Array();
+var xml_linenames = new Array();
+
 for (var i = 0; i < root.childElementCount; i++) {
     line = root.children[i];
     line_id = line.getAttribute("i")
     line_name = line.getAttribute("lb");
     line_color = "#" + line.getAttribute("lc").slice(2);  // 线路颜色
     line_colors[line_name] = line_color;
-    // 画路径，存paths，stations
+
+    // 算linename
+    for (let lps of line.getAttribute("lp").split(';')) {
+        let lp = lps.split(',');
+        xml_linenames.push(
+            `<rect width="${lp[2]}" height="${lp[3]}" fill="${line_color}" x="${lp[0]}" y="${lp[1]}" />` +
+            `<text class="line-name" x="${add(lp[0], parseFloat(lp[2]) / 2)}" y="${add(lp[1], parseFloat(lp[3]) / 2)}" >${line_name}</text>`
+        );
+    }
+
+
+
     // 环线
     if (line.getAttribute("loop") === "true") {
         line_child_count = line.childElementCount + 1;
@@ -55,11 +121,12 @@ for (var i = 0; i < root.childElementCount; i++) {
     }
 
     paths[line_name] = new Array();
-    paths[line_name].push(new Array());
-    paths[line_name].push(new Array());
+    paths[line_name].push(new Array());  // 正序
+    paths[line_name].push(new Array());  // 倒序
     stations[line_name] = new Array();
-    stations[line_name].push(new Array());
-    stations[line_name].push(new Array());
+    stations[line_name].push(new Array());  // 正序
+    stations[line_name].push(new Array());  // 倒序
+    stations_xy[line_name] = new Object();
 
     d = `M${line.children[0].getAttribute("x")},${line.children[0].getAttribute("y")}`; // 起点
     for (var j = 1; j < line_child_count; j++) {
@@ -80,7 +147,8 @@ for (var i = 0; i < root.childElementCount; i++) {
         }
 
         paths[line_name][0].push(d);
-        xml_paths.push(`<path stroke="${line_color}" fill="none" d="${d}"></path>`)
+        xml_paths.push(`<path class="line" stroke="${line_color}" d="${d}" />`)
+
         d = `M${end_p.getAttribute("x")},${end_p.getAttribute("y")}`; // 下个起点
 
     }
@@ -113,28 +181,64 @@ for (var i = 0; i < root.childElementCount; i++) {
         paths[line_name][1].push(t.join(" "));
     }
     paths[line_name][1].reverse();
-    // TODO：画站
+    // 算站
     for (var j = 0; j < line.childElementCount; j++) {
         if (line.children[j].getAttribute("st") === "true") {
             stations[line_name][0].push(line.children[j].getAttribute("lb"));
             stations[line_name][1].unshift(line.children[j].getAttribute("lb"));
+            let x = line.children[j].getAttribute("x");
+            let y = line.children[j].getAttribute("y");
+            stations_xy[line_name][line.children[j].getAttribute("lb")] = [x, y];
+            if (line.children[j].getAttribute("ex") === "true") {
+                // 换乘车站
+                if (!ex_stations_xy[line.children[j].getAttribute("lb")]) {
+                    ex_stations_xy[line.children[j].getAttribute("lb")] = new Array();
+                    // 画站名
+                    xml_stationnames.push(`<text class="station-name" x="${add(x, line.children[j].getAttribute("rx"))}" y="${add(y, line.children[j].getAttribute("ry")) + 15}">${line.children[j].getAttribute("lb")}</text>`)
+                }
+                ex_stations_xy[line.children[j].getAttribute("lb")].push([x, y]);
+
+            }
+            else {
+                // 普通车站
+
+                // 画普通车站和站名
+                xml_stations.push(`<circle class="station-normal" cx="${x}" cy="${y}" />`);
+                xml_stationnames.push(`<text class="station-name" x="${add(x, line.children[j].getAttribute("rx"))}" y="${add(y, line.children[j].getAttribute("ry")) + 15}">${line.children[j].getAttribute("lb")}</text>`)
+            }
+
+
         }
     }
-
-
 }
 
+// 画换乘站
+
+for (let ex_station in ex_stations_xy) {
+    let new_x = 0;
+    let new_y = 0;
+    let count = 0;
+    for (let [x, y] of ex_stations_xy[ex_station]) {
+        new_x += parseFloat(x);
+        new_y += parseFloat(y);
+        count += 1;
+    }
+    new_x /= count;
+    new_y /= count;
+    //xml_stations.push(`<circle class="station-transfer" cx="${new_x}" cy="${new_y}"></circle>`);
+    xml_stations.push(`<use xlink:href="#station-transfer" x="${new_x - 5.5}" y="${new_y - 5.5}" />`);
+}
 //console.log(stations);
 //console.log(paths);
 
 
 var xhr_sche_wd = new XMLHttpRequest();
-xhr_sche_wd.open("GET", "static/schedule_weekday.json", false);
+xhr_sche_wd.open("GET", "data/schedule_weekday.json", false);
 xhr_sche_wd.send();
 sche_wd = JSON.parse(xhr_sche_wd.responseText);
 
 var xhr_sche_we = new XMLHttpRequest();
-xhr_sche_we.open("GET", "static/schedule_weekend.json", false);
+xhr_sche_we.open("GET", "data/schedule_weekend.json", false);
 xhr_sche_we.send();
 sche_we = JSON.parse(xhr_sche_we.responseText);
 
@@ -200,26 +304,32 @@ function set_time(begin_minute, end_minute, start_minute, speed) {
 }
 
 // 画地图，无车
-function draw_map() {
+function draw_map(is_hide_station, is_hide_linename, is_hide_stationname) {
     div_svg = d3.select('#div_svg');
     div_svg.html('');
     svg = div_svg.append('svg')
         .attr('width', 2000)
         .attr('height', 2000)
         .attr('xmlns', 'http://www.w3.org/2000/svg');
-    svg.html(xml_paths.join(''));
+    var svg_html = '';
+    svg_html += svg_style;
+    svg_html += xml_paths.join('');
+    if (!is_hide_station) svg_html += xml_stations.join('');
+    if (!is_hide_linename) svg_html += xml_linenames.join('');
+    if (!is_hide_stationname) svg_html += xml_stationnames.join('');
+    svg.html(svg_html);
 }
 
 
 // 画车动画
-function draw_trains(begin_minute, end_minute, speed, day) {
+function draw_trains(begin_minute, end_minute, speed, wde, is_lines, train_size, is_hide_linename, is_hide_stationname, is_hide_station) {
     transparency_second = 0.5;
     xml_polygons = new Array();
 
-    if (day === 'wd') {
+    if (wde === 'wd') {
         sche = sche_wd;
     }
-    else if (day === 'we') {
+    else if (wde === 'we') {
         sche = sche_we;
     }
     else {
@@ -229,11 +339,10 @@ function draw_trains(begin_minute, end_minute, speed, day) {
     for (line_name in sche) {
         direct_index = -1;
         for (direct in sche[line_name]) {
+            if (!is_lines[line_name][direct]) continue;  // 隐藏某线路的车
             direct_index += 1;
-            console.log(direct_index, direct);
             for (train_num in sche[line_name][direct]) {
                 xml_animates = new Array();
-
                 // 淡入
                 this_time_minute = hm2time(sche[line_name][direct][train_num][0][1]);
                 if (end_minute <= this_time_minute) {
@@ -302,7 +411,9 @@ function draw_trains(begin_minute, end_minute, speed, day) {
                 if (begin_minute <= next_time_minute && end_minute >= next_time_minute) {
                     xml_animates.push(`<animate begin="${begin}" dur="${dur}" attributeName="opacity" values="1;0" repeatCount="1" />`)
                 }
-                xml_polygon = `<polygon id="T_${train_num}" points="0,0 9,0 3,6 -9,6 -9,0" stroke-width="2" fill="grey" stroke="#790000">${xml_animates.join('')}</polygon>`;
+                var t = train_size;
+                let points = `0,0 ${t * 3},0 ${t},${t * 2} -${t * 3},${t * 2} -${t * 3},0`;
+                xml_polygon = `<polygon id="T_${train_num}" fill="${line_colors[line_name]}" points="${points}" stroke-width="${t / 2}" stroke="#000000">${xml_animates.join('')}</polygon>`;
                 xml_polygons.push(xml_polygon);
             }
         }
@@ -317,7 +428,16 @@ function draw_trains(begin_minute, end_minute, speed, day) {
         .attr('width', 2000)
         .attr('height', 2000)
         .attr('xmlns', 'http://www.w3.org/2000/svg');
-    svg.html(xml_paths.join('') + xml_polygons.join(''));
+
+    var svg_html = '';
+    svg_html += svg_style;
+    svg_html += xml_paths.join('');
+    if (!is_hide_station) svg_html += xml_stations.join('');
+    if (!is_hide_linename) svg_html += xml_linenames.join('');
+    if (!is_hide_stationname) svg_html += xml_stationnames.join('');
+    svg_html += xml_polygons.join('');
+
+    svg.html(svg_html);
     start_set_time(begin_minute, end_minute, get_now_minute(), speed);
 
 }
